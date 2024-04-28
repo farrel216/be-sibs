3b9ece5836bbaf4ea8f55dc40e89d7da4e541bf0
Farrel Andhika <farrel.arp@gmail.com>
2024-04-28 13:19:18 +0700

    delete dist folder

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./application/server");
-const port = process.env.PORT || 5000;
-server_1.server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
